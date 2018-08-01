f91a21baf324b1bd8fc78315ec1a9251e92d7fdb
aiwe <maksymivv@gmail.com>
2018-08-01 16:14:00 +0300

    Update pools

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ var networkStat = {
 		["krb.crypto-coins.club", "http://krb.crypto-coins.club:8118/stats"],
 		["krb.sberex.com", "http://krb.sberex.com:7006/stats"],
 		["krb.cryptomine.pro", "http://krb.cryptomine.pro:8117/stats"],
-		["krb-pool.pp.ua", "http://krb-pool.pp.ua:25417/stats"],
+		["karbo-pool.pp.ua", "http://karbo-pool.pp.ua:25417/stats"],
 		["easyhash.io/pools/krb", "https://api-krb.easyhash.io/stats"],
 		["krbpool.ml", "http://krbpool.ml:8117/stats"],
 		["krb.kopanka.com", "http://kopanka.com:8117/stats"],
@@ -38,8 +38,8 @@ var networkStat = {
 		["krb.pool.ualinux.com", "http://krb.pool.ualinux.com:8117/stats"],
 		["krb.agilepools.com", "https://krb.agilepools.com/api/stats"],
 		["krb.fastpool.eu", "http://krb.fastpool.eu:8118/stats"],
-		["krbpool.pp.ua", "https://krbpool.pp.ua:8118/stats"],
 		["karbo.luckypool.org", "https://karbo.luckypool.org/api/stats"],
+		["krbpool.pp.ua", "https://krbpool.pp.ua:8118/stats"],
 		["krb.freshpool.org", "https://krb.freshpool.org:13001/stats"],
 		["krb.tfoto.com.ua", "http://178.150.34.202:8117/stats"],
 		["pool.mineallcrypto.com", "http://pool.mineallcrypto.com/pool/stats/?symbol=KRB"],
@@ -50,6 +50,13 @@ var networkStat = {
 		["cryptoknight.cc/karbo", "https://cryptoknight.cc/rpc/karbo/stats"],
 		["mineallcrypto.com/karbowanec", "https://mineallcrypto.com/karbowanec/pool/stats"],
 		["krb.irdpool.ru", "http://krb.irdpool.ru:8112/stats"],
-		["krb.simplypool.net", "http://85.10.204.245:1117/stats"]		
+		["krb.simplypool.net", "http://85.10.204.245:1117/stats"],
+		["krb.x-i-tech.com", "http://krb.x-i-tech.com:8117/stats"],
+		["krb.sintezpool.com", "http://206.189.168.140:8117/stats"],
+		["karbo.fairhash.org", "https://karbo.fairhash.org/api/stats"],
+		["karbo.herominers.com", "https://karbo.herominers.com/api/stats"],
+		["krb.hashgang.io", "https://eu1.krb.hashgang.io/api/stats"],
+		["easyhash.pro/krb", "https://easyhash.pro/krb/api/stats"],
+		["krb.mychainpools.com", "https://krb.mychainpools.com:8148/stats"]
     ]
 };
