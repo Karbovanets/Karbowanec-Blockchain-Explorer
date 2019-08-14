3eff3c0269b199fcbebf8a781f3a2cae4b5bb977
aiwe <maksymivv@gmail.com>
2019-08-14 17:05:42 +0300

    Fix blocks per page

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ var blockTargetInterval = 240;
 var coinUnits = 1000000000000;
 var symbol = 'KRB';
 var refreshDelay = 30000;
-var blockPerPage = 10;
+var blocksPerPage = 10;
 var whiteTheme = "css/themes/white-theme.css";
 var nightTheme = "css/themes/dark-theme.css";
 // pools stats by MainCoins
