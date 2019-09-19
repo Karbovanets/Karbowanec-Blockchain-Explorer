0833275681b47177bb366bb773d888c9e6e34988
aiwe <maksymivv@gmail.com>
2019-09-19 22:07:34 +0300

    Update api queries to use getinfo

--- q/reward/index.php
+++ q/reward/index.php
@@ -1,7 +1,5 @@
 <?php
 require '../util.php';
 $config = (require '../../config.php');
-$blockHeader = fetch_rpc($config['api'], 'getlastblockheader', '""');
-$rewardRaw = $blockHeader['result']['block_header']['reward'];
-$reward = number_format($rewardRaw / $config['coinUnits'], $config['coinDecimals'], ".", "");
-print_r($reward);
+$info = fetch_getinfo($config['api']);
+print_r($info['next_reward']);
