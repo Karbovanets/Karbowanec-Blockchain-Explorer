c99af23df56841f47da3edd13c34b55f67fb9a99
aiwe <maksymivv@gmail.com>
2021-02-09 19:05:14 +0200

    Update reserve proof for an address

--- api/submit_richlist.php
+++ api/submit_richlist.php
@@ -13,10 +13,25 @@
    
         $json = file_get_contents($file);
         $arr = json_decode($json, true);
+
         if (in_array($formDecoded , $arr)) {
-            echo '{"result": "Already exists"}';
+            exit('{"result": "Already exists"}');
+        }
+
+        $i = array_search($formDecoded['address'], array_column($arr, 'address'));
+
+        if (false !== $i) {
+            unset($arr[$i]); // delete old record
+            array_push($arr, $formDecoded); // add new
+            $json = json_encode($arr, JSON_PRETTY_PRINT);
+            if (file_put_contents($file, $json)) {
+                echo '{"result": "Richlist entry updated successfuly"}';
+            }
+            else {
+                echo '{"result": "Error putting entry to file"}';
+            }
         } else {
-            array_push($arr, $formDecoded);
+            array_push($arr, $formDecoded); // add new
             $json = json_encode($arr, JSON_PRETTY_PRINT);
             if (file_put_contents($file, $json)) {
                 echo '{"result": "Richlist entry submitted successfuly"}';
