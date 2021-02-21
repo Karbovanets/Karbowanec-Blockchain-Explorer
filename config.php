a9dce762493961d6742cc21d7999468242accc9d
Pavel Z <r3volut1oner@gmail.com>
2021-02-21 13:13:15 +0200

    Docker file for setup the explorer

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 $node = 'https://karbo.club/services/node_web';
 return array(
-	'api' => 'https://karbo.club/services/node_web',
+	'api' => $node,
 	'blockTargetInterval' => 240,
 	'coinUnits' => 1000000000000,
 	'coinDecimals' => 12
