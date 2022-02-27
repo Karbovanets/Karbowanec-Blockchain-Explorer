4468b74dd400aff457fd27f1d864264c245be22b
Aiwe <maksymivv@gmail.com>
2022-02-27 21:17:16 +0200

    Decode extra nonce to a readable text if any

--- js/main.js
+++ js/main.js
@@ -272,6 +272,43 @@ function toHexString(byteArray) {
         (output + ('0' + elem.toString(16)).slice(-2)), '');
 }
 
+/* utf.js - UTF-8 <=> UTF-16 convertion
+ *
+ * Copyright (C) 1999 Masanao Izumo <iz@onicos.co.jp>
+ */
+function Utf8ArrayToStr(array) {
+    var out, i, len, c;
+    var char2, char3;
+
+    out = "";
+    len = array.length;
+    i = 0;
+    while (i < len) {
+        c = array[i++];
+        switch (c >> 4)
+        { 
+        case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7:
+            // 0xxxxxxx
+            out += String.fromCharCode(c);
+            break;
+        case 12: case 13:
+            // 110x xxxx   10xx xxxx
+            char2 = array[i++];
+            out += String.fromCharCode(((c & 0x1F) << 6) | (char2 & 0x3F));
+            break;
+        case 14:
+            // 1110 xxxx  10xx xxxx  10xx xxxx
+            char2 = array[i++];
+            char3 = array[i++];
+            out += String.fromCharCode(((c & 0x0F) << 12) |
+                                    ((char2 & 0x3F) << 6) |
+                                    ((char3 & 0x3F) << 0));
+            break;
+        }
+    }    
+    return out;
+}
+
 function handleSearch() {
 
     var text = document.getElementById('txt_search').value;
